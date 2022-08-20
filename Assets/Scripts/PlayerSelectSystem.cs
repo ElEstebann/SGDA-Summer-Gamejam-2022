c80cd52977317ed4acdef589c0b5dfd8c1d6887b
Cilmering <Sebamar02@gmail.com>
2022-08-19 23:22:26 -0400

    Can now scroll through character sprites in Player Select

--- Assets/Scripts/PlayerSelectSystem.cs
+++ Assets/Scripts/PlayerSelectSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSelectSystem : MonoBehaviour {
     private enum ControlSelected {
@@ -15,6 +16,16 @@ public class PlayerSelectSystem : MonoBehaviour {
     public Sprite[] player2Sprites = new Sprite[4];
     public Sprite[] player3Sprites = new Sprite[4];
     public Sprite[] player4Sprites = new Sprite[4];
+
+    public Image player1CharacterDisplay;
+    public Image player2CharacterDisplay;
+    public Image player3CharacterDisplay;
+    public Image player4CharacterDisplay;
+
+    public int player1CurrentSpriteDisplayed;
+    public int player2CurrentSpriteDisplayed;
+    public int player3CurrentSpriteDisplayed;
+    public int player4CurrentSpriteDisplayed;
 
     private void SelectControlType(int player, ControlSelected control) {
         if (player == 0) {
@@ -62,10 +73,73 @@ public class PlayerSelectSystem : MonoBehaviour {
         }
     }
 
+    public void ScrollCharacterDisplayLeft(int player) {
+        if (player == 0) {
+            player1CurrentSpriteDisplayed -= 1;
+            if (player1CurrentSpriteDisplayed < 0) {
+                player1CurrentSpriteDisplayed = 3;
+            }
+            player1CharacterDisplay.sprite = player1Sprites[player1CurrentSpriteDisplayed];
+        } else if (player == 1) {
+            player2CurrentSpriteDisplayed -= 1;
+            if (player2CurrentSpriteDisplayed < 0) {
+                player2CurrentSpriteDisplayed = 3;
+            }
+            player2CharacterDisplay.sprite = player2Sprites[player2CurrentSpriteDisplayed];
+        } else if (player == 2) {
+            player3CurrentSpriteDisplayed -= 1;
+            if (player3CurrentSpriteDisplayed < 0) {
+                player3CurrentSpriteDisplayed = 3;
+            }
+            player3CharacterDisplay.sprite = player3Sprites[player3CurrentSpriteDisplayed];
+        } else if (player == 3) {
+            player4CurrentSpriteDisplayed -= 1;
+            if (player4CurrentSpriteDisplayed < 0) {
+                player4CurrentSpriteDisplayed = 3;
+            }
+            player4CharacterDisplay.sprite = player4Sprites[player4CurrentSpriteDisplayed];
+        }
+    }
+
+    public void ScrollCharacterDisplayRight(int player) {
+        if (player == 0) {
+            player1CurrentSpriteDisplayed += 1;
+            if (player1CurrentSpriteDisplayed > 3) {
+                player1CurrentSpriteDisplayed = 0;
+            }
+            player1CharacterDisplay.sprite = player1Sprites[player1CurrentSpriteDisplayed];
+        } else if (player == 1) {
+            player2CurrentSpriteDisplayed += 1;
+            if (player2CurrentSpriteDisplayed > 3) {
+                player2CurrentSpriteDisplayed = 0;
+            }
+            player2CharacterDisplay.sprite = player2Sprites[player2CurrentSpriteDisplayed];
+        } else if (player == 2) {
+            player3CurrentSpriteDisplayed += 1;
+            if (player3CurrentSpriteDisplayed > 3) {
+                player3CurrentSpriteDisplayed = 0;
+            }
+            player3CharacterDisplay.sprite = player3Sprites[player3CurrentSpriteDisplayed];
+        } else if (player == 3) {
+            player4CurrentSpriteDisplayed += 1;
+            if (player4CurrentSpriteDisplayed > 3) {
+                player4CurrentSpriteDisplayed = 0;
+            }
+            player4CharacterDisplay.sprite = player4Sprites[player4CurrentSpriteDisplayed];
+        }
+    }
+
     private void Start() {
         multiplayerManager = FindObjectOfType<MultiplayerManager>();
 
+        player1CurrentSpriteDisplayed = 0;
+        player2CurrentSpriteDisplayed = 0;
+        player3CurrentSpriteDisplayed = 0;
+        player4CurrentSpriteDisplayed = 0;
+
         // Set the options currently selected to whatever is in the multiplayer manager!
+
+
 
     }
 }
